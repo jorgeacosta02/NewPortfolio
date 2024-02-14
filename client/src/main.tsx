68fd7c1a82205ba1a9a34769e0c6541ebb0a6fb4
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-14 15:20:43 -0300

    redux toolkit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './_main.module.scss'
 import axios from 'axios'
+
+import './_main.module.scss'
+import { store } from './redux/store/store.ts'
 
 axios.defaults.baseURL = 'http://localhost:3600'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ client/src/redux/actions/actions.ts
@@ -0,0 +1,30 @@
+// // actions.ts
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { IUserDataFromDB } from '../Interfaces/userInterfaces';
+// import { IWorkerDataFromDB } from '../Interfaces/workerInterfaces';
+
+
+// export const loginUser: any = createAsyncThunk('user/loginUser', async (credentials: { dni: number; password: string }) => {
+//   try {
+//     const response = await axios.post<IUserDataFromDB, any>('/user-login', credentials);
+//     console.log('response.data en loginUser: ', response.data);
+//     const data = response.data;
+//     localStorage.setItem('accessLogin', JSON.stringify(data));
+//     return data;
+//   } catch (error) {
+//     throw new Error('Error en la solicitud de inicio de sesión');
+//   }
+// });
+
+// export const loginWorker: any = createAsyncThunk('worker/loginWorker', async (credentials: { dni: number; password: string }) => {
+//   try {
+//     const response = await axios.post<IWorkerDataFromDB, any>('/worker-login', credentials);
+//     console.log('response.data en loginWorker: ', response.data);
+//     const data = response.data;
+//     localStorage.setItem('accessLogin', JSON.stringify(data));
+//     return data;
+//   } catch (error) {
+//     throw new Error('Error en la solicitud de inicio de sesión');
+//   }
+// });
