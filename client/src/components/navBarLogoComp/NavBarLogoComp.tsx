39107bcd226b041f6d75af422cffcda918fe00a2
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-14 23:21:05 -0300

    navBar styles

--- client/src/components/navBarLogoComp/NavBarLogoComp.tsx
+++ client/src/components/navBarLogoComp/NavBarLogoComp.tsx
@@ -4,6 +4,9 @@ import styles from './_NavBarLogoComp.module.scss';
 const LogoComp = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.logo}>
+        Portfolio
+      </h1>
     </div>
   )
 }
