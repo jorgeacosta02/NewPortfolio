c092573f82cac399a0a343651de8940896cb1187
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-14 23:53:46 -0300

    animation

--- client/src/components/navBarLogoComp/NavBarLogoComp.tsx
+++ client/src/components/navBarLogoComp/NavBarLogoComp.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import styles from './_NavBarLogoComp.module.scss';
 
+const NavBarLogoComp = () => {
+    const [animationComplete, setAnimationComplete] = useState(false);
 
-const LogoComp = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.logo}>
-        Portfolio
-      </h1>
-    </div>
-  )
+    const onAnimationEnd = () => {
+        setAnimationComplete(true);
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1 className={`${styles.logo} ${animationComplete && styles.animationComplete}`} onAnimationEnd={onAnimationEnd}>
+                Portfolio
+            </h1>
+        </div>
+    );
 }
 
-export default LogoComp
+export default NavBarLogoComp;
