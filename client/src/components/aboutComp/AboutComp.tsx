69520c19fe8c9eff3814a12526fedb96ba8937a0
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-02-21 12:40:53 -0300

    Using SliderComp

--- client/src/components/aboutComp/AboutComp.tsx
+++ client/src/components/aboutComp/AboutComp.tsx
@@ -1,89 +1,22 @@
 import styles from './_AboutComp.module.scss'
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectMoonState } from '../../redux/slices/moonSlice';
 import { selectLangState } from '../../redux/slices/langSlice';
-
-// Importa tus imágenes
-import img01 from '../../assets/images/slider/code01.jpg';
-import img02 from '../../assets/images/slider/laptop01.jpg';
-import img03 from '../../assets/images/slider/code02.jpg';
-import img04 from '../../assets/images/slider/laptop02.jpg';
-import img05 from '../../assets/images/slider/code03.jpg';
-import img06 from '../../assets/images/slider/laptop03.jpg';
-import img07 from '../../assets/images/slider/code04.jpg';
-import img08 from '../../assets/images/slider/laptop04.jpg';
-import img09 from '../../assets/images/slider/code05.jpg';
-import img10 from '../../assets/images/slider/laptop05.jpg';
-import img11 from '../../assets/images/slider/code06.jpg';
-import img12 from '../../assets/images/slider/laptop06.jpg';
-// Other images
 import ProfileImg from '../../assets/images/profile/Profile.jpg'
+import SliderComp from '../sliderComp/SliderComp';
 
 
 const AboutComp = () => {
   
   const moonState = useSelector(selectMoonState);
 
-  console.log(moonState)
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % 12);
-    }, 2000); // Cambia a la siguiente imagen cada 3 segundos
-
-    return () => clearInterval(interval);
-  }, []); // Se ejecuta solo una vez al montar el componente
-
-  console.log(currentSlide)
-
-  // Define un arreglo con las imágenes
-  const images = [
-    img01,
-    img02,
-    img03,
-    img04,
-    img05,
-    img06,
-    img07,
-    img08,
-    img09,
-    img10,
-    img11,
-    img12
-  ];
-
-  const backColor = `${styles.background} ${moonState.moon ? styles.bckgndWhite : ''}`;
-  
   const containerColor = `${styles.container} ${moonState.moon ? styles.containerWhite : ''}`;
 
   const langState = useSelector(selectLangState).lang;
 
   return (
     <div className={containerColor}>
-
-        {/* Background slider */}
-        <div className={styles.slider}>
-            {images.map((image, index) => (
-            <div
-            className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
-                key={index}
-                >
-                <img
-                src={image}
-                alt=""
-                className={styles.img}
-                />
-            </div>
-            ))}
-        </div>
-
-        {/* Background slider cover */}
-        <div className={backColor}></div>
-
+        <SliderComp/>
         {/* Comment */}
         <div className={styles.commentContainer}>
           <p className={styles.comment}>
--- client/src/components/projectsComp/ProjectsComp.tsx
+++ client/src/components/projectsComp/ProjectsComp.tsx
@@ -1,68 +1,19 @@
 import styles from './_ProjectsComp.module.scss'
-
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectMoonState } from '../../redux/slices/moonSlice';
 import { selectLangState } from '../../redux/slices/langSlice';
 import { NavLink } from 'react-router-dom';
-
-// Importa tus imágenes
-import img01 from '../../assets/images/slider/code01.jpg';
-import img02 from '../../assets/images/slider/laptop01.jpg';
-import img03 from '../../assets/images/slider/code02.jpg';
-import img04 from '../../assets/images/slider/laptop02.jpg';
-import img05 from '../../assets/images/slider/code03.jpg';
-import img06 from '../../assets/images/slider/laptop03.jpg';
-import img07 from '../../assets/images/slider/code04.jpg';
-import img08 from '../../assets/images/slider/laptop04.jpg';
-import img09 from '../../assets/images/slider/code05.jpg';
-import img10 from '../../assets/images/slider/laptop05.jpg';
-import img11 from '../../assets/images/slider/code06.jpg';
-import img12 from '../../assets/images/slider/laptop06.jpg';
-
 import triarLogo from '../../assets/images/logos&Brands/TriarLogo.png'
 import exploserviceLogoBrand from '../../assets/images/logos&Brands/ExploserviceLogoBrand.png'
 import craftBeerLogo from '../../assets/images/logos&Brands/craftBeerLogo.jpg'
 import videogamesImage from '../../assets/images/logos&Brands/Videogames.png'
+import SliderComp from '../sliderComp/SliderComp';
 
 
 const ProjectsComp = () => {
 
   const moonState = useSelector(selectMoonState);
 
-  console.log(moonState)
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prevSlide) => (prevSlide + 1) % 12);
-    }, 2000); // Cambia a la siguiente imagen cada 3 segundos
-
-    return () => clearInterval(interval);
-  }, []); // Se ejecuta solo una vez al montar el componente
-
-  console.log(currentSlide)
-
-  // Define un arreglo con las imágenes
-  const images = [
-    img01,
-    img02,
-    img03,
-    img04,
-    img05,
-    img06,
-    img07,
-    img08,
-    img09,
-    img10,
-    img11,
-    img12
-  ];
-
-  const backColor = `${styles.background} ${moonState.moon ? styles.backWhite : ''}`;
-  
   const containerColor = `${styles.container} ${moonState.moon ? styles.containerWhite : ''}`;
 
   const projectWhite = `${styles.project} ${moonState.moon ? styles.projectWhite : ''}`;
@@ -71,27 +22,8 @@ const ProjectsComp = () => {
 
   return (
     <div className={containerColor}>
-
-      {/* Background slider */}
-      <div className={styles.slider}>
-            {images.map((image, index) => (
-            <div
-            className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
-                key={index}
-                >
-                <img
-                src={image}
-                alt=""
-                className={styles.img}
-                />
-            </div>
-            ))}
-        </div>
-
-        {/* Background slider cover */}
-        <div className={backColor}></div>
-
-        {/* Projects */}
+        <SliderComp/>
+           {/* Projects */}
         <ul className={styles.projectsContainer}>
             <li
               key="1"
